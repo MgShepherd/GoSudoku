8916e6c48c5d7e9d9577097fcdd86a1cc2e25a11
MgShepherd <michaelshep1504@gmail.com>
2025-10-31 18:08:20 +0000

    Setup display for sudoku grid

--- generator.go
+++ generator.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func generate() {
-	fmt.Println("This function will be used to generate a Sudoku")
+const GRID_SIZE = 3
+
+type Sudoku struct {
+	grid [][]int
+}
+
+func newSudoku() *Sudoku {
+	grid := make([][]int, GRID_SIZE*GRID_SIZE)
+	for i := range grid {
+		grid[i] = make([]int, GRID_SIZE*GRID_SIZE)
+	}
+	grid[2][2] = 4
+	grid[4][4] = 9
+	return &Sudoku{grid}
+}
+
+func (s *Sudoku) String() string {
+	var sb strings.Builder
+	sb.WriteRune('\n')
+	for i := range GRID_SIZE {
+		sb.WriteString("+-------+-------+-------+\n")
+		for j := range GRID_SIZE {
+			writeElementLine(&sb, s.grid[(i*GRID_SIZE)+j])
+		}
+	}
+	sb.WriteString("+-------+-------+-------+\n")
+	return sb.String()
+}
+
+func writeElementLine(sb *strings.Builder, line []int) {
+	startPos := 0
+	for range GRID_SIZE {
+		sb.WriteString("| ")
+		for i := range GRID_SIZE {
+			el := line[startPos+i]
+			if el == 0 {
+				sb.WriteString(". ")
+			} else {
+				fmt.Fprintf(sb, "%d ", el)
+			}
+		}
+		startPos += GRID_SIZE
+	}
+	sb.WriteString("|\n")
 }
--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
-	generate()
+	sudoku := newSudoku()
+	fmt.Println(sudoku)
 }
